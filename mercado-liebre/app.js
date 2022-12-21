0f18a0dd346a0b46eb9f23ddacc720525152a48b
Pablo Baleztena <pablo@ultharsoftware.com>
2022-12-21 18:46:02 -0300

    clase 21

--- mercado-liebre/app.js
+++ mercado-liebre/app.js
@@ -12,3 +12,11 @@ app.listen(3001, () => {
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "views/home.html"));
 });
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "views/register.html"));
+});
+
+app.post("/login", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "views/login.html"));
+});

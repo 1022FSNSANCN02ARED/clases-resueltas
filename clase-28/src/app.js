52860a08fabcb60980a823dc0330527acd8f06c2
Pablo Baleztena <pablo@ultharsoftware.com>
2023-01-23 17:17:04 -0300

    clase-28 final

--- clase-28/src/app.js
+++ clase-28/src/app.js
@@ -37,14 +37,14 @@ app.use((req, res, next) => next(createError(404)));
 
 // ************ error handler ************
 app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.path = req.path;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.path = req.path;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
 });
 
 // ************ exports app - dont'touch ************
--- clase-28/src/controllers/productsController.js
+++ clase-28/src/controllers/productsController.js
@@ -1,40 +1,43 @@
 const products = require("../data/products");
 
 const controller = {
-  // Root - Show all products
-  index: (req, res) => {
-    // Do the magic
-  },
+    // Root - Show all products
+    index: (req, res) => {
+        res.render("products/list", { products });
+    },
 
-  // Detail - Detail from one product
-  detail: (req, res) => {
-    // Do the magic
-  },
+    // Detail - Detail from one product
+    detail: (req, res) => {
+        const product = products.find((p) => p.id == req.params.id);
+        res.render("products/detail", { product });
+    },
 
-  // Create - Form to create
-  create: (req, res) => {
-    res.render("product-create-form");
-  },
+    // Create - Form to create
+    create: (req, res) => {
+        res.render("products/create-form");
+    },
 
-  // Create -  Method to store
-  store: (req, res) => {
-    const product = req.body;
-    res.send(product);
-  },
+    // Create -  Method to store
+    store: (req, res) => {
+        const product = req.body;
+        res.send(product);
+    },
 
-  // Update - Form to edit
-  edit: (req, res) => {
-    // Do the magic
-  },
-  // Update - Method to update
-  update: (req, res) => {
-    // Do the magic
-  },
+    // Update - Form to edit
+    edit: (req, res) => {
+        const product = products.find((p) => p.id == req.params.id);
+        res.render("products/edit-form", { product });
+    },
+    // Update - Method to update
+    update: (req, res) => {
+        const product = req.body;
+        res.send(product);
+    },
 
-  // Delete - Delete one product from DB
-  destroy: (req, res) => {
-    // Do the magic
-  },
+    // Delete - Delete one product from DB
+    destroy: (req, res) => {
+        res.send(`deleting ${req.params.id}`);
+    },
 };
 
 module.exports = controller;

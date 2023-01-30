edbbf168911ee307360f93eea9ddaedc5362fbd5
Pablo Baleztena <pablo@ultharsoftware.com>
2023-01-30 18:52:36 -0300

    materiales clase 31

---
+++ clase-31/src/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+const mainRouter = require("./routes/main-router");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use("/", mainRouter);
+
+const PORT = process.env.PORT ?? 3001;
+app.listen(PORT);

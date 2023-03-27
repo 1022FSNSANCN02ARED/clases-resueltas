052912a0020a4686afab1bb6647085ef8b38489c
Pablo Baleztena <pablo@ultharsoftware.com>
2023-03-27 18:59:37 -0300

    clase 45

---
+++ clase-45/src/app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const path = require("path");
+const methodOverride = require("method-override");
+
+const mainRouter = require("./routes/index");
+const dayjs = require("dayjs");
+const app = express();
+
+app.locals = {
+  formatDate: (date) => {
+    if (!date) return "";
+    return dayjs(date).format("YYYY-MM-DD");
+  },
+};
+
+app.use(express.json());
+app.set("views", path.resolve(__dirname, "./views"));
+app.set("view engine", "ejs");
+
+app.use(express.static(path.resolve(__dirname, "../public")));
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(methodOverride("_method"));
+
+app.use(mainRouter);
+
+app.listen("3001", () => console.log("Servidor corriendo en el puerto 3001"));
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});

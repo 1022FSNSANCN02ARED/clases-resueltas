a8702f70a71fdf112a2791f5a47fed2feddd4b89
Pablo Baleztena <pablo@ultharsoftware.com>
2023-04-12 21:34:40 -0300

    clase-51-final

--- clase-51/src/components/GenresList.jsx
+++ clase-51/src/components/GenresList.jsx
@@ -1,70 +1,23 @@
 import React from "react";
+import GenreItem from "./GenreItem.jsx";
+import BigCard from "./BigCard.jsx";
+
+const genres = [
+  { id: 1, name: "Acción" },
+  { id: 2, name: "Animación" },
+  { id: 3, name: "Aventura" },
+  { id: 4, name: "Ciencia Ficción" },
+];
 
 function GenresList() {
   return (
-    <div className="col-lg-6 mb-4">
-      <div className="card shadow mb-4">
-        <div className="card-header py-3">
-          <h5 className="m-0 font-weight-bold text-gray-800">
-            Genres in Data Base
-          </h5>
-        </div>
-        <div className="card-body">
-          <div className="row">
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Acción</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Animación</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Aventura</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Ciencia Ficción</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Comedia</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Documental</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Drama</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Fantasia</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Infantiles</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Musical</div>
-              </div>
-            </div>
-          </div>
-        </div>
+    <BigCard title="Genres in Data Base">
+      <div className="row">
+        {genres.map((genre) => {
+          return <GenreItem key={genre.id} name={genre.name} />;
+        })}
       </div>
-    </div>
+    </BigCard>
   );
 }
 
--- clase-51/src/components/Stats.jsx
+++ clase-51/src/components/Stats.jsx
@@ -1,65 +1,49 @@
 import React from "react";
+import SmallCard from "./SmallCard.jsx";
+
+const myStats = [
+  {
+    id: 2,
+    title: "Movies in Data Base",
+    value: "21",
+    color: "primary",
+    icon: "fa-film",
+  },
+  {
+    id: 3,
+    title: "Actors quantity",
+    value: "49",
+    color: "warning",
+    icon: "fa-user",
+  },
+  {
+    id: 1,
+    title: "Total awards",
+    value: "79",
+    color: "success",
+    icon: "fa-award",
+  },
+];
 
 function Stats() {
   return (
     <div className="row">
-      {/* <!-- Movies in Data Base --> */}
-      <div className="col-md-4 mb-4">
-        <div className="card border-left-primary shadow h-100 py-2">
-          <div className="card-body">
-            <div className="row no-gutters align-items-center">
-              <div className="col mr-2">
-                <div className="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                  Movies in Data Base
-                </div>
-                <div className="h5 mb-0 font-weight-bold text-gray-800">21</div>
-              </div>
-              <div className="col-auto">
-                <i className="fas fa-film fa-2x text-gray-300"></i>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      {/* <!-- Total awards --> */}
-      <div className="col-md-4 mb-4">
-        <div className="card border-left-success shadow h-100 py-2">
-          <div className="card-body">
-            <div className="row no-gutters align-items-center">
-              <div className="col mr-2">
-                <div className="text-xs font-weight-bold text-success text-uppercase mb-1">
-                  {" "}
-                  Total awards
-                </div>
-                <div className="h5 mb-0 font-weight-bold text-gray-800">79</div>
-              </div>
-              <div className="col-auto">
-                <i className="fas fa-award fa-2x text-gray-300"></i>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      {/* <!-- Actors quantity --> */}
-      <div className="col-md-4 mb-4">
-        <div className="card border-left-warning shadow h-100 py-2">
-          <div className="card-body">
-            <div className="row no-gutters align-items-center">
-              <div className="col mr-2">
-                <div className="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                  Actors quantity
-                </div>
-                <div className="h5 mb-0 font-weight-bold text-gray-800">49</div>
-              </div>
-              <div className="col-auto">
-                <i className="fas fa-user fa-2x text-gray-300"></i>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      {myStats.map((stat) => {
+        return (
+          <SmallCard
+            key={stat.id}
+            title={stat.title}
+            value={stat.value}
+            color={stat.color}
+            icon={stat.icon}
+          />
+          // <SmallCard
+          //   key={stat.id}
+          //   {...stat}
+          // />
+        );
+      })}
+      {/* [ SmallCard,SmallCard,SmallCard ] */}
     </div>
   );
 }
